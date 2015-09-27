ed3cbec10e2d7f7c1501a71cbe462579a4d11a19
wegitor <wegitor@gmail.com>
2015-09-27 19:54:07 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    system("calc & pause");
+    cout << "Press the enter key to continue ...";
+    cin.get();
+    return EXIT_SUCCESS;
+}
